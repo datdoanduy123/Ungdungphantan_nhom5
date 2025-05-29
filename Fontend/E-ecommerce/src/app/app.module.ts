c7a9bf7d84f41e3d8b4d87253c7f3afb532c6700
South294 <banhgatongonngon@gmail.com>
2025-05-29 07:58:48 +0700

    Update app.module.ts

--- Fontend/E-ecommerce/src/app/app.module.ts
+++ Fontend/E-ecommerce/src/app/app.module.ts
@@ -1,1 +1,16 @@
+import { HttpClientModule } from '@angular/common/http';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ProductListComponent 
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule 
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
 
