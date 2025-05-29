e35bb661943bf8061262218ef92de670b3faf1b7
South294 <banhgatongonngon@gmail.com>
2025-05-29 08:10:28 +0700

    Update product.service.ts

--- Fontend/E-ecommerce/src/app/services/product.service.ts
+++ Fontend/E-ecommerce/src/app/services/product.service.ts
@@ -1,1 +1,28 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+export interface Product {
+  id: number;
+  name: string;
+  price: number;
+  category: string;
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductService {
+  private apiUrl = 'http://localhost:8222/apt/v1/products'; 
+
+  constructor(private http: HttpClient) {}
+
+  getProducts(): Observable<Product[]> {
+    return this.http.get<Product[]>(this.apiUrl);
+  }
+
+  searchProducts(name: string): Observable<Product[]> {
+    return this.http.get<Product[]>(`${this.apiUrl}?name=${name}`);
+  }
+}
 
