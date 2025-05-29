48c4006bce1ea663cc30a1c3967f0af7c6b8e8ea
South294 <banhgatongonngon@gmail.com>
2025-05-29 07:56:12 +0700

    Update app.routes.ts

--- Fontend/E-ecommerce/src/app/app.routes.ts
+++ Fontend/E-ecommerce/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { ProductListComponent } from './components/product-list/product-list.component';
+import { OrderComponent } from './components/order/order.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: '/products', pathMatch: 'full' },
+  { path: 'products', component: ProductListComponent },
+  { path: 'order', component: OrderComponent }
+];
